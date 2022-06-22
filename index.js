d811c3d47759a33f791199299e7f4e1c89ccda8e
Emily Reed <emilyreed89@gmail.com>
2022-06-21 20:39:37 -0400

    lemonade popsicles are yum

--- index.js
+++ index.js
@@ -61,7 +61,15 @@ app
     });
 });
 
-//
+//DELETE
+
+app.route('/remove/:id').get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if(err) return res.send(500, err);
+        res.redirect('/');
+    });
+});
 
 
 
